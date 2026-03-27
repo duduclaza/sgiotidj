5a46be41f3be7e6b5579845ace56b5cd346d79f3
Eduardo TI UAI <contato@tiiuai.com.br>
2026-03-27 04:08:20 -0300

    botao

--- homologacoes_mock/nova_homologacao.php
+++ homologacoes_mock/nova_homologacao.php
@@ -1,13 +1,6 @@
 <?php
 require_once __DIR__ . '/init.php';
 
-// Tratar a troca de usuário no mock (Global para o módulo mock)
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['trocar_usuario'])) {
-    $_SESSION['usuario_logado_id'] = (int)$_POST['usuario_logado_id'];
-    header("Location: " . $_SERVER['REQUEST_URI']);
-    exit;
-}
-
 $u = getUsuarioLogado();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['criar_homologacao'])) {
